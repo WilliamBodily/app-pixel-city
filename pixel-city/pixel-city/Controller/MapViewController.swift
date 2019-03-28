a274ba35907a8af7a4f36d1830e060de76f30a13
William Bodily <william.bodily@gmail.com>
2019-03-28 17:47:06 -0400

    Built out the user interface for the application

--- pixel-city/pixel-city/Controller/MapViewController.swift
+++ pixel-city/pixel-city/Controller/MapViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
 
-
+    @IBAction func centerMapButtonPressed(_ sender: Any) {
+    }
+    
 }
 
+extension MapViewController: MKMapViewDelegate {
+    
+}
